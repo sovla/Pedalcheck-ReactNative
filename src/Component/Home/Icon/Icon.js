3663a181100da0de43c37f27a2744f02b3f84220
sovla <sovla@naver.com>
2021-12-21 11:45:34 +0900

    Google Login

--- src/Component/Home/Icon/Icon.js
+++ src/Component/Home/Icon/Icon.js
@@ -7,30 +7,30 @@ import Naver from '@assets/image/ic_naver.png';
 import Apple from '@assets/image/ic_apple.png';
 import DefaultImage from '@/assets/global/Image';
 
-export function KakaoImage() {
+export function KakaoImage({onPress}) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={onPress}>
       <DefaultImage source={Kakao} width="60px" height="60px" resizeMode="contain" />
     </TouchableOpacity>
   );
 }
-export function GoogleImage() {
+export function GoogleImage({onPress}) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={onPress}>
       <DefaultImage source={Google} width="60px" height="60px" resizeMode="contain" />
     </TouchableOpacity>
   );
 }
-export function NaverImage() {
+export function NaverImage({onPress}) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={onPress}>
       <DefaultImage source={Naver} width="60px" height="60px" resizeMode="contain" />
     </TouchableOpacity>
   );
 }
-export function AppleImage() {
+export function AppleImage({onPress}) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={onPress}>
       <DefaultImage source={Apple} width="60px" height="60px" resizeMode="contain" />
     </TouchableOpacity>
   );
--- src/Page/Home/Login.js
+++ src/Page/Home/Login.js
@@ -1,5 +1,5 @@
-import {LinkButton} from '@/assets/global/Button';
-import {Box, Container, ScrollBox} from '@/assets/global/Container';
+import {LinkButton, TextLinkButton} from '@/assets/global/Button';
+import {BetweenBox, Box, Container, ScrollBox} from '@/assets/global/Container';
 import {DefaultInput} from '@/assets/global/Input';
 import HomeFooter from '@/Component/Home/HomeFooter';
 import LogoBox from '@/Component/Home/LogoBox';
@@ -7,6 +7,7 @@ import React, {useState} from 'react';
 import {useSelector} from 'react-redux';
 import {KeyboardAvoidingView} from 'react-native';
 import {getPixel} from '@/Util/pixelChange';
+import {AppleImage, GoogleImage, KakaoImage, NaverImage} from '@/Component/Home/Icon/Icon';
 
 export default function Login({navigation}) {
   const size = useSelector(state => state.size);
@@ -53,6 +54,20 @@ export default function Login({navigation}) {
               width={size.minusPadding}
               height="44px"></LinkButton>
           </Box>
+        </Box>
+        <Box alignItems="center">
+          <Box pd="16px">
+            <TextLinkButton
+              to={() => navigation.navigate('Register')}
+              content="SNS 계정으로 회원가입/로그인"
+            />
+          </Box>
+          <BetweenBox width={`${size.designWidth - 100}px`} pd="0px 0px 10px">
+            <KakaoImage />
+            <GoogleImage />
+            <NaverImage />
+            <AppleImage />
+          </BetweenBox>
         </Box>
       </ScrollBox>
       <HomeFooter isAbsolute navigation={navigation} isShowLogin={false} />
