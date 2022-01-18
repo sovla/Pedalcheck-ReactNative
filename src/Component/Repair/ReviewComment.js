4b6c055d913e19a07d630b3ead1e770f6cea3d07
dlgusxo1000 <dlgusxo1000@gmail.com>
2022-01-18 11:50:14 +0900

    220118

--- src/Component/Repair/ReviewComment.js
+++ src/Component/Repair/ReviewComment.js
@@ -1,11 +1,13 @@
-import {Box, Container, PositionBox, RowBox} from '@/assets/global/Container';
-import {DarkBoldText, DarkText, DefaultText} from '@/assets/global/Text';
+import {BetweenBox, Box, Container, PositionBox, RowBox} from '@/assets/global/Container';
+import {DarkBoldText, DarkMediumText, DarkText, DefaultText} from '@/assets/global/Text';
 import Theme from '@/assets/global/Theme';
 import {DefaultTheme} from '@react-navigation/native';
 import React from 'react';
 import {View, Text} from 'react-native';
 import ReplyIcon from '@assets/image/ic_reply.png';
 import DefaultImage from '@/assets/global/Image';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {BorderButton} from '@/assets/global/Button';
 
 export default function ReviewComment({
   size,
@@ -24,14 +26,27 @@ export default function ReviewComment({
       pd="10px 10px 10px 44px"
       backgroundColor={Theme.color.backgroundBlue}
       borderRadius="10px">
-      <RowBox backgroundColor="#0000" alignItems="center">
-        <DarkBoldText mg="0px 5px 0px 0px" fontSize={Theme.fontSize.fs15}>
-          {name}
-        </DarkBoldText>
-        <DefaultText color={Theme.color.gray} fontSize={Theme.fontSize.fs12}>
-          {reviewDate.slice(0, 10)}
-        </DefaultText>
-      </RowBox>
+      <BetweenBox backgroundColor="#0000" alignItems="center">
+        <RowBox>
+          <DarkBoldText mg="0px 5px 0px 0px" fontSize={Theme.fontSize.fs15}>
+            {name}
+          </DarkBoldText>
+          <DefaultText color={Theme.color.gray} fontSize={Theme.fontSize.fs12}>
+            {reviewDate.slice(0, 10)}
+          </DefaultText>
+        </RowBox>
+        <TouchableOpacity>
+          <Box mg="0px 5px">
+            <BorderButton
+              width="38px"
+              height="25px"
+              borderColor={Theme.borderColor.gray}
+              color={Theme.color.black}>
+              <DarkMediumText fontSize={Theme.fontSize.fs13}>수정</DarkMediumText>
+            </BorderButton>
+          </Box>
+        </TouchableOpacity>
+      </BetweenBox>
       <RowBox backgroundColor="#0000">
         <DefaultText
           numberOfLines={isDetailPage ? 50 : 3}
