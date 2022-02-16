1fed013dc86d407629c51cdd23c18757fd437ee8
sovla <sovla@naver.com>
2022-02-16 16:02:08 +0900

    데이트 피커 수정

--- src/Component/Home/Modal/DatePicker.js
+++ src/Component/Home/Modal/DatePicker.js
@@ -1,39 +1,27 @@
-import {RowBox} from '@/assets/global/Container';
-import React, {useCallback, useEffect, useState} from 'react';
+import {PositionBox, RowBox} from '@/assets/global/Container';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import ModalTitleBox from '../../Modal/ModalTitleBox';
 import {LinkWhiteButton} from '@/assets/global/Button';
 import {modalClose} from '@/Store/modalState';
-import {DatePicker, Picker} from 'react-native-wheel-pick';
-import {SetBirthDate} from '@/Store/birthDateState';
-import {getHeightPixel, getPixel} from '@/Util/pixelChange';
-import {parse} from '@babel/core';
-import {useLayoutEffect} from 'react';
 
+import {SetBirthDate} from '@/Store/birthDateState';
+import {useLayoutEffect} from 'react';
+import DatePicker from 'react-native-date-picker';
+import useUpdateEffect from '@/Hooks/useUpdateEffect';
+import {numberChangeFormat} from '@Util/numberFormat';
 export default function BirthDatePicker({birth, setBirth}) {
   const now = new Date();
+
   const [birthDate, setBirthDate] = useState({
-    year,
-    month,
-    day,
+    year: now.getFullYear,
+    month: now.getMonth() + 1,
+    day: now.getDate(),
   });
-  const [day, setDay] = useState(getBetweenNumber(1, new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()));
-  const [isRender, setIsRender] = useState(false);
+  const [dateDummy, setDateDummy] = useState(new Date());
   const size = useSelector(state => state.size);
   const dispatch = useDispatch();
 
-  const year = getBetweenNumber(1900, new Date().getFullYear());
-  const month = getBetweenNumber(1, 12);
-
-  const ChangeYear = (value, key) => {
-    if (isRender) {
-      setBirthDate(prev => {
-        return {...prev, [key]: value};
-      });
-    }
-  };
-
-  const pickerWidth = getPixel((size.designWidth - 100) / 4);
   const onPressConfirm = () => {
     if (setBirth) {
       setBirth(birthDate);
@@ -41,70 +29,42 @@ export default function BirthDatePicker({birth, setBirth}) {
     dispatch(SetBirthDate({year: birthDate.year, month: birthDate.month, day: birthDate.day}));
     dispatch(modalClose());
   };
-  useEffect(() => {
-    if (birthDate?.year > 1900) setDay(getBetweenNumber(1, new Date(birthDate?.year, birthDate?.month, 0).getDate()));
-  }, [birthDate.month]);
+
+  useUpdateEffect(() => {
+    setBirthDate({
+      year: dateDummy.getFullYear(),
+      month: dateDummy.getMonth() + 1,
+      day: dateDummy.getDate(),
+    });
+  }, [dateDummy]);
 
   useLayoutEffect(() => {
     if (birth) {
+      setDateDummy(new Date(`${birth.year}-${numberChangeFormat(birth.month)}-${numberChangeFormat(birth.day)}`));
       setBirthDate({
-        year: parseInt(birth.year),
-        month: parseInt(birth.month),
-        day: parseInt(birth.day),
+        year: birth.year,
+        month: birth.month,
+        day: birth.day,
       });
     }
-    setIsRender(true);
   }, []);
 
-  if (day.length === 0) {
-    return null;
-  }
   return (
     <>
       <ModalTitleBox size={size} title="날짜 선택" padding={32} />
-      <RowBox mg="15px 0px 30px">
-        <Picker
-          style={{backgroundColor: 'white', width: pickerWidth, height: getHeightPixel(120)}}
-          value={birthDate.year}
-          onValueChange={value => ChangeYear(value, 'year')}
-          pickerData={year}
-          itemSpace={getPixel(40)}
-          selectedValue={birthDate?.year ?? now.getFullYear()}
-        />
-        <Picker
-          style={{backgroundColor: 'white', width: pickerWidth, height: getHeightPixel(120)}}
-          value={birthDate.month}
-          onValueChange={value => ChangeYear(value, 'month')}
-          pickerData={month}
-          itemSpace={getPixel(40)}
-          selectedValue={birthDate?.month ?? now.getMonth() + 1}
-        />
-        <Picker
-          style={{backgroundColor: 'white', width: pickerWidth, height: getHeightPixel(120)}}
-          value={birthDate.day}
-          onValueChange={value => ChangeYear(value, 'day')}
-          pickerData={day}
-          itemSpace={getPixel(40)}
-          selectedValue={birthDate?.day ?? now.getDate()}
-        />
-        <Picker
-          style={{backgroundColor: 'white', width: pickerWidth, height: getHeightPixel(120)}}
-          value={birthDate}
-          onValueChange={() => {}}
-          pickerData={['양력', '음력']}
-          itemSpace={getPixel(40)}
-          selectedValue={now.getFullYear()}
+      <RowBox>
+        <DatePicker
+          date={dateDummy}
+          onDateChange={date => {
+            setDateDummy(date);
+          }}
+          mode="date"
+          locale="ko"
+          maximumDate={new Date()}
+          minimumDate={new Date('1900-01-01')}
         />
       </RowBox>
       <LinkWhiteButton content="확인" to={onPressConfirm} />
     </>
   );
 }
-
-const getBetweenNumber = (startNum, endNum) => {
-  let result = [];
-  for (let index = 0; startNum + index <= endNum; index++) {
-    result.push(startNum + index);
-  }
-  return result;
-};
--- src/Component/Home/Modal/RepairDatePicker.js
+++ src/Component/Home/Modal/RepairDatePicker.js
@@ -1,92 +1,63 @@
-import {RowBox} from '@/assets/global/Container';
+import {Box, PositionBox, RowBox} from '@/assets/global/Container';
 import React, {useCallback, useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import ModalTitleBox from '../../Modal/ModalTitleBox';
 import {LinkWhiteButton} from '@/assets/global/Button';
 import {modalClose} from '@/Store/modalState';
-import {DatePicker, Picker} from 'react-native-wheel-pick';
-import {SetBirthDate} from '@/Store/birthDateState';
+import DatePicker from 'react-native-date-picker';
 import {getHeightPixel, getPixel} from '@/Util/pixelChange';
 import {parse} from '@babel/core';
 import {useLayoutEffect} from 'react';
+import {numberChangeFormat} from '@/Util/numberFormat';
 
 export default function RepairDatePicker({birth, setBirth}) {
   const now = new Date();
-  const [birthDate, setBirthDate] = useState({
-    year,
-    month,
-    day,
-  });
-  const [day, setDay] = useState(getBetweenNumber(1, new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()));
-  const [isRender, setIsRender] = useState(false);
   const size = useSelector(state => state.size);
   const dispatch = useDispatch();
 
-  const year = getBetweenNumber(1900, new Date().getFullYear());
-  const month = getBetweenNumber(1, 12);
+  const [date, setDate] = useState(new Date());
 
-  const ChangeYear = (value, key) => {
-    if (isRender) {
-      setBirthDate(prev => {
-        return {...prev, [key]: value};
-      });
-    }
-  };
-
-  const pickerWidth = getPixel((size.designWidth - 100) / 4);
   const onPressConfirm = () => {
     if (setBirth) {
-      setBirth(new Date(birthDate.year, birthDate.month - 1));
+      setBirth(date);
     }
     dispatch(modalClose());
   };
-  useEffect(() => {
-    if (birthDate?.year > 1900) setDay(getBetweenNumber(1, new Date(birthDate?.year, birthDate?.month, 0).getDate()));
-  }, [birthDate.month]);
 
   useLayoutEffect(() => {
     if (birth) {
-      setBirthDate({
-        year: parseInt(birth.year),
-        month: parseInt(birth.month),
-      });
+      console.log(birth);
     }
-    setIsRender(true);
   }, []);
-
-  if (day.length === 0) {
-    return null;
-  }
   return (
     <>
       <ModalTitleBox size={size} title="날짜 선택" padding={32} />
-      <RowBox mg="15px 0px 30px">
-        <Picker
-          style={{backgroundColor: 'white', width: pickerWidth, height: getHeightPixel(120)}}
-          value={birthDate.year}
-          onValueChange={value => ChangeYear(value, 'year')}
-          pickerData={year}
-          itemSpace={getPixel(40)}
-          selectedValue={birthDate?.year ?? now.getFullYear()}
+      <RowBox mg="15px 0px 30px" alignItems="center" justifyContent="center">
+        <Box width="100px" />
+        <DatePicker
+          date={date}
+          onDateChange={selectDate => {
+            setDate(selectDate);
+          }}
+          mode="date"
+          maximumDate={
+            new Date(
+              `${now.getFullYear() + 3}-${numberChangeFormat(now.getMonth() + 1)}-${numberChangeFormat(now.getDate())}`,
+            )
+          }
+          minimumDate={
+            new Date(
+              `${now.getFullYear() - 3}-${numberChangeFormat(now.getMonth() + 1)}-${numberChangeFormat(now.getDate())}`,
+            )
+          }
+          style={{
+            maxWidth: 400,
+          }}
         />
-        <Picker
-          style={{backgroundColor: 'white', width: pickerWidth, height: getHeightPixel(120)}}
-          value={birthDate.month}
-          onValueChange={value => ChangeYear(value, 'month')}
-          pickerData={month}
-          itemSpace={getPixel(40)}
-          selectedValue={birthDate?.month ?? now.getMonth() + 1}
-        />
+        <PositionBox top="0px" left="80px" width="60px" height="100%" backgroundColor="#fff" zIndex={1000} />
+        <PositionBox top="0px" right="0px" width="120px" height="100%" backgroundColor="#fff" zIndex={1000} />
       </RowBox>
       <LinkWhiteButton content="확인" to={onPressConfirm} />
     </>
   );
 }
-
-const getBetweenNumber = (startNum, endNum) => {
-  let result = [];
-  for (let index = 0; startNum + index <= endNum; index++) {
-    result.push(startNum + index);
-  }
-  return result;
-};
