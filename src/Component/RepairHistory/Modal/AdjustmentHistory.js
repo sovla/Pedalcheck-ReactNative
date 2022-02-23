76b0e18759e5fe91744496d54cc7b46576853a73
dlgusxo1000 <dlgusxo1000@gmail.com>
2022-02-23 13:57:27 +0900

    220223

--- src/Component/RepairHistory/Modal/AdjustmentHistory.js
+++ src/Component/RepairHistory/Modal/AdjustmentHistory.js
@@ -1,12 +1,13 @@
 import {BetweenBox, Box, RowBox} from '@/assets/global/Container';
-import {DarkBoldText, DarkMediumText, DarkText, GrayText, IndigoText} from '@/assets/global/Text';
+import {DarkBoldText, DarkMediumText, DarkText, GrayText, IndigoText, MoneyText} from '@/assets/global/Text';
 import Theme from '@/assets/global/Theme';
 import {borderBottomWhiteGray} from '@/Component/BikeManagement/ShopRepairHistory';
 import ModalTitleBox from '@/Component/Modal/ModalTitleBox';
 import React from 'react';
 import {View, Text} from 'react-native';
 
-export default function AdjustmentHistory() {
+export default function AdjustmentHistory({item}) {
+  console.log('item ::: ', item);
   return (
     <>
       <ModalTitleBox title="정산내역" />
@@ -14,49 +15,73 @@ export default function AdjustmentHistory() {
         <BetweenBox mg="10px 0px 0px" width="100%">
           <DarkMediumText>정산금액</DarkMediumText>
           <RowBox alignItems="center">
-            <IndigoText fontWeight={Theme.fontWeight.bold}>100,000</IndigoText>
-            <DarkText>원</DarkText>
+            <MoneyText color={Theme.color.indigo} fontWeight={Theme.fontWeight.bold} money={item.clt_tot_price} />
           </RowBox>
         </BetweenBox>
         <BetweenBox mg="10px 0px 0px" width="100%">
-          <GrayText fontSize={Theme.fontSize.fs14}>PG사 수수료 차감(10%)</GrayText>
-          <GrayText fontSize={Theme.fontSize.fs14}>-1,000원</GrayText>
+          <GrayText fontSize={Theme.fontSize.fs14}>PG사 수수료 차감({item.clt_pg_percent}%)</GrayText>
+          <GrayText fontSize={Theme.fontSize.fs14}>
+            -<MoneyText fontSize={Theme.fontSize.fs14} money={item.clt_pg_price} color={Theme.color.gray} />
+          </GrayText>
+        </BetweenBox>
+        <BetweenBox mg="10px 0px 0px" width="100%">
+          <GrayText fontSize={Theme.fontSize.fs14}>PG사 수수료 부가세</GrayText>
+          <GrayText fontSize={Theme.fontSize.fs14}>
+            -<MoneyText fontSize={Theme.fontSize.fs14} money={item.clt_pg_vat} color={Theme.color.gray} />
+          </GrayText>
         </BetweenBox>
         <BetweenBox mg="10px 0px 0px" width="100%">
           <DarkBoldText>PG사 수수료 차감액</DarkBoldText>
 
           <RowBox alignItems="center">
-            <DarkBoldText>99,000</DarkBoldText>
-            <DarkText>원</DarkText>
+            <MoneyText
+              money={+item.clt_pg_vat + +item.clt_pg_price}
+              color={Theme.color.black}
+              fontWeight={Theme.fontWeight.bold}
+            />
           </RowBox>
         </BetweenBox>
         <BetweenBox mg="10px 0px 0px" width="100%">
           <GrayText fontSize={Theme.fontSize.fs14}>
-            페달체크 수수료 차감 {'\n'}(PG사 수수료 차감액에서 5%)
+            페달체크 수수료 차감 {'\n'}(PG사 수수료 차감액에서 {item.clt_pc_percent}%)
           </GrayText>
-          <GrayText fontSize={Theme.fontSize.fs14}>-4,950원</GrayText>
+          <GrayText fontSize={Theme.fontSize.fs14}>
+            {' '}
+            -<MoneyText fontSize={Theme.fontSize.fs14} money={item.clt_pc_price} color={Theme.color.gray} />
+          </GrayText>
         </BetweenBox>
+
         <BetweenBox mg="10px 0px 0px" width="100%">
+          <GrayText fontSize={Theme.fontSize.fs14}>페달체크 부가세</GrayText>
+          <GrayText fontSize={Theme.fontSize.fs14}>
+            {' '}
+            -<MoneyText fontSize={Theme.fontSize.fs14} money={item.clt_pc_vat} color={Theme.color.gray} />
+          </GrayText>
+        </BetweenBox>
+
+        <BetweenBox mg="10px 0px 20px" width="100%">
           <DarkBoldText>페달체크 수수료 차감액</DarkBoldText>
 
           <RowBox alignItems="center">
-            <DarkBoldText>94,050</DarkBoldText>
-            <DarkText>원</DarkText>
+            <MoneyText
+              money={+item.clt_pc_vat + +item.clt_pc_price}
+              color={Theme.color.black}
+              fontWeight={Theme.fontWeight.bold}
+            />
           </RowBox>
         </BetweenBox>
-        <BetweenBox mg="10px 0px 20px" width="100%">
+        {/* <BetweenBox mg="10px 0px 20px" width="100%">
           <GrayText fontSize={Theme.fontSize.fs14}>
             원천세 차감{'\n'}
             (페달체크 수수료 차감액에서 3.3%)
           </GrayText>
           <GrayText fontSize={Theme.fontSize.fs14}>-3,130원</GrayText>
-        </BetweenBox>
+        </BetweenBox> */}
       </Box>
       <BetweenBox mg="20px 0px 0px" width="328px">
         <DarkMediumText>정산 실수령액</DarkMediumText>
         <RowBox alignItems="center">
-          <IndigoText fontWeight={Theme.fontWeight.bold}>90,947</IndigoText>
-          <DarkText>원</DarkText>
+          <MoneyText fontWeight={Theme.fontWeight.bold} color={Theme.color.indigo} money={item.clt_price} />
         </RowBox>
       </BetweenBox>
     </>
