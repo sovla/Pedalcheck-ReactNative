c43e84ca8c7b01bc8f3190c876c46e36ca52b4fd
sovla <sovla@naver.com>
2022-06-02 11:25:04 +0900

    - 사업자등록번호 펼쳐서 보기 작업완료 - 당일 예약건 안내 메시지 추가 완료

--- src/Page/Repair/ReservationDate.js
+++ src/Page/Repair/ReservationDate.js
@@ -20,115 +20,101 @@ import {AlertButton} from '@/Util/Alert';
 import Loading from '@/Component/Layout/Loading';
 
 export default function ReservationDate({navigation, route: {params}}) {
-  const isFocused = useIsFocused();
-  const dispatch = useDispatch();
-  const now = new Date(moment().format('YYYY-MM-DD HH:mm:ss'));
+    const isFocused = useIsFocused();
+    const dispatch = useDispatch();
+    const now = new Date(moment().format('YYYY-MM-DD HH:mm:ss'));
 
-  const {shopInfo, reservationInfo} = useSelector(state => state);
-  const [selectItem, setSelectItem] = useState(''); // 선택한 시간
-  const [selectDate, setSelectDate] = useState(new Date().toISOString().substring(0, 10)); // 선택한 날짜
+    const {shopInfo, reservationInfo} = useSelector(state => state);
+    const [selectItem, setSelectItem] = useState(''); // 선택한 시간
+    const [selectDate, setSelectDate] = useState(new Date().toISOString().substring(0, 10)); // 선택한 날짜
 
-  const [disabledDayList, setDisabledDayList] = useState([]); // 선택불가 날짜
-  const [disabledTimeList, setDisabledTimeList] = useState([]); // 선택불가 시간
-  const [timeList, setTimeList] = useState([]); // 선택가능한 시간
+    const [disabledDayList, setDisabledDayList] = useState([]); // 선택불가 날짜
+    const [disabledTimeList, setDisabledTimeList] = useState([]); // 선택불가 시간
+    const [timeList, setTimeList] = useState([]); // 선택가능한 시간
 
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setDisabledTimeList([]);
-    setTimeList([]);
-    setSelectItem('');
-    setIsLoading(true);
-    getReservationTimeList({
-      ot_pt_date: selectDate,
-      mt_idx: shopInfo?.store_info?.mt_idx,
-    })
-      .then(res => res.data?.result === 'true' && res.data.data?.data)
-      .then(data => {
-        setDisabledTimeList(data?.order_time);
-        if (Array.isArray(data?.store_time)) {
-          const result = data.store_time
+    useEffect(() => {
+        setDisabledTimeList([]);
+        setTimeList([]);
+        setSelectItem('');
+        setIsLoading(true);
+        getReservationTimeList({
+            ot_pt_date: selectDate,
+            mt_idx: shopInfo?.store_info?.mt_idx,
+        })
+            .then(res => res.data?.result === 'true' && res.data.data?.data)
+            .then(data => {
+                setDisabledTimeList(data?.order_time);
+                if (Array.isArray(data?.store_time)) {
+                    const result = data.store_time
 
-            .filter(item => item.flag === 'Y')
-            .reduce((prev, curr) => {
-              if (!prev?.st_time) {
-                return [...prev, curr.st_time];
-              } else {
-                return [curr.st_time];
-              }
+                        .filter(item => item.flag === 'Y')
+                        .reduce((prev, curr) => {
+                            if (!prev?.st_time) {
+                                return [...prev, curr.st_time];
+                            } else {
+                                return [curr.st_time];
+                            }
+                        });
+                    setTimeList(result);
+                }
+            })
+            .finally(() => {
+                setIsLoading(false);
             });
-          setTimeList(result);
+    }, [selectDate]);
+
+    useEffect(() => {
+        if (isFocused) {
+            onChangeMonth(selectDate?.substr(0, 7) ?? `${now.getFullYear()}-${now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1}`);
         }
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [selectDate]);
+    }, [isFocused]);
 
-  useEffect(() => {
-    if (isFocused) {
-      onChangeMonth(
-        selectDate?.substr(0, 7) ??
-          `${now.getFullYear()}-${now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1}`,
-      );
-    }
-  }, [isFocused]);
+    const onChangeMonth = day => {
+        setIsLoading(true);
+        getdisabledReservationDayList({
+            ot_pt_month: day,
+            mt_idx: shopInfo?.store_info?.mt_idx,
+        })
+            .then(res => res.data.result === 'true' && res.data.data.data)
+            .then(data => setDisabledDayList(data.disabled_date))
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
 
-  const onChangeMonth = day => {
-    setIsLoading(true);
-    getdisabledReservationDayList({
-      ot_pt_month: day,
-      mt_idx: shopInfo?.store_info?.mt_idx,
-    })
-      .then(res => res.data.result === 'true' && res.data.data.data)
-      .then(data => setDisabledDayList(data.disabled_date))
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+    const onPressNext = () => {
+        if (selectDate && selectItem) {
+            dispatch(
+                setReservationDate({
+                    date: selectDate,
+                    time: selectItem,
+                }),
+            );
+            navigation.navigate('ReservationRequest');
+        } else {
+            AlertButton('예약 시간을 선택해주세요.');
+        }
+    };
 
-  const onPressNext = () => {
-    if (selectDate && selectItem) {
-      dispatch(
-        setReservationDate({
-          date: selectDate,
-          time: selectItem,
-        }),
-      );
-      navigation.navigate('ReservationRequest');
-    } else {
-      AlertButton('예약 시간을 선택해주세요.');
-    }
-  };
-
-  return (
-    <>
-      {isLoading && <Loading isAbsolute />}
-      <Header title="정비예약" />
-      <Box flex={1}>
-        <ScrollBox flex={1} keyboardShouldPersistTaps="handled">
-          <RepairReservationHeader step={3} />
-          <DefaultLine height="10px" backgroundColor={Theme.borderColor.whiteLine} />
-          <Box mg="20px 16px">
-            <ReservationCalendar
-              selectDate={selectDate}
-              setSelectDate={setSelectDate}
-              onChangeMonth={onChangeMonth}
-              disabledDayList={disabledDayList}
-            />
-          </Box>
-          <TimeList
-            selectDate={selectDate}
-            timeList={timeList}
-            disabled={disabledTimeList}
-            selectItem={selectItem}
-            setSelectItem={setSelectItem}
-          />
-          <Box mg="20px 16px" height="44px">
-            <LinkButton to={() => onPressNext()} content="다음"></LinkButton>
-          </Box>
-        </ScrollBox>
-      </Box>
-    </>
-  );
+    return (
+        <>
+            {isLoading && <Loading isAbsolute />}
+            <Header title="정비예약" />
+            <Box flex={1}>
+                <ScrollBox flex={1} keyboardShouldPersistTaps="handled">
+                    <RepairReservationHeader step={3} />
+                    <DefaultLine height="10px" backgroundColor={Theme.borderColor.whiteLine} />
+                    <Box mg="20px 16px">
+                        <ReservationCalendar selectDate={selectDate} setSelectDate={setSelectDate} onChangeMonth={onChangeMonth} disabledDayList={disabledDayList} />
+                    </Box>
+                    <TimeList selectDate={selectDate} timeList={timeList} disabled={disabledTimeList} selectItem={selectItem} setSelectItem={setSelectItem} />
+                    <Box mg="20px 16px" height="44px">
+                        <LinkButton to={() => onPressNext()} content="다음"></LinkButton>
+                    </Box>
+                </ScrollBox>
+            </Box>
+        </>
+    );
 }
